078a07bc760170c207878fe4e210bdb542fb46db
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2024-12-30 11:01:20 +0530

    login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'signup',
+        component:SignupComponent
+    },
+    {
+        path: 'login',
+        component:LoginComponent
+    },
+];
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { LucideAngularModule, Scissors } from 'lucide-angular';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { LucideAngularModule, Scissors, Lock, Mail } from 'lucide-angular';
 
 
 @Component({
   selector: 'app-login',
   imports: [
-    LucideAngularModule
+    LucideAngularModule,
+    RouterLink,
+    RouterOutlet
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   readonly ScissorsIcon = Scissors;
+  readonly Lock = Lock;
+  readonly Mail = Mail;
 }

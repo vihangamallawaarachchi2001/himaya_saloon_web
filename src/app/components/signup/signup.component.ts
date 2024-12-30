b008abffdc295a62b43af70cf8650a2c4e21256b
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2024-12-30 11:33:09 +0530

    signup: ui

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LucideAngularModule, Scissors, User, Mail,Phone, Lock } from 'lucide-angular';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [LucideAngularModule, RouterOutlet],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
 export class SignupComponent {
+  readonly ScissorsIcon = Scissors;
+  readonly UserIcon = User;
+  readonly MailIcon = Mail;
+  readonly PhoneIcon = Phone;
+  readonly LockIcon = Lock;
 
 }

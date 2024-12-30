d92459084e65af1e8bac136c62a4ba0660737d42
Vihanga Mallawaarachchi <vihanganethusara00@gmail.com>
2024-12-30 22:17:06 +0530

    dashboar: reschool

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { ProfileComponent } from "../../components/profile/profile.component";
 import { RecentAppoitmentsComponent } from "../../components/recent-appoitments/recent-appoitments.component";
-import { RecentNotificationsComponent } from "../../components/recent-notifications/recent-notifications.component";
 import { RateComponent } from "../../components/rate/rate.component";
 import { RecentPurchasesComponent } from "../../components/recent-purchases/recent-purchases.component";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ProfileComponent, RecentAppoitmentsComponent, RecentNotificationsComponent, RateComponent, RecentPurchasesComponent],
+  imports: [ RecentAppoitmentsComponent, RateComponent, RecentPurchasesComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
